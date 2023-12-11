e692b960d0701de699e4b38bbe32677b71cc20dc
Elijahtab <elijah.tabachnik@gmail.com>
2023-12-10 23:07:55 -0800

    Added Chunks of Forest that is Modifiable

--- Assets/Scripts/Data/NoiseData.cs
+++ Assets/Scripts/Data/NoiseData.cs
@@ -12,14 +12,28 @@ public class NoiseData : UpdatableData
 	[Range(0,1)]
 	public float persistance;
 	public float lacunarity;
-
 	public Vector2 offset;
+
+
+	public float treeNoiseScale;
+	public int treeOctaves;
+	[Range(0,1)]
+	public float treePersistance;
+	public float treeLacunarity;
+	public Vector2 treeOffset;
+	
     protected override void OnValidate(){
         if (lacunarity < 1) {
 			lacunarity = 1;
 		}
 		if (octaves < 0) {
 			octaves = 0;
+		}
+		if (treeLacunarity < 1) {
+			treeLacunarity = 1;
+		}
+		if (treeOctaves < 0) {
+			treeOctaves = 0;
 		}
         base.OnValidate ();
     }
--- Assets/Scripts/Terrain/ObjectGenerator.cs
+++ Assets/Scripts/Terrain/ObjectGenerator.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class ObjectGenerator : MonoBehaviour
 {
     public TerrainData terrainData;
+    public NoiseData noiseData;
+    public int seed;
     public GameObject prefabTest;
     public LayerMask layerMask;
     [Range(0,1)]
     public float treeStartHeight = 0;
     [Range(0,1)]
     public float treeStopHeight = 0.4f;
-    [Range(0,1)] 
+    [Range(0,.1f)] 
     public float numOfTrees = 0.5f;
+    const int mapChunkSize = 111;
+    [Range(0,1)]
+    public float treeChunkSize = .5f;
 
 
     public void DeleteAll()
@@ -21,6 +27,7 @@ public class ObjectGenerator : MonoBehaviour
     }
     public void GenerateObjects(float[,] heightMap)
     {
+        float[,] treeNoiseMap = Noise.GenerateNoiseMap (mapChunkSize, mapChunkSize, seed, noiseData.treeNoiseScale, noiseData.treeOctaves, noiseData.treePersistance, noiseData.treeLacunarity, noiseData.treeOffset);
         // Destroy foliageParent if it already exists
         if (GameObject.Find("FoliageParent"))
         {
@@ -39,13 +46,15 @@ public class ObjectGenerator : MonoBehaviour
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
-            {
+            {         
                 int originalX = x / uniformScale;
                 int originalY = y / uniformScale;
                 float currentHeight = heightMap[originalX, originalY];
+                float currentTreeHeight = treeNoiseMap[originalX, originalY];  
 
-                if (currentHeight >= treeStartHeight && currentHeight <= treeStopHeight)
+                if (currentHeight >= treeStartHeight && currentHeight <= treeStopHeight && currentTreeHeight <= treeChunkSize)
                 {
+                    
                     if (Random.value < numOfTrees)
                     {
                         Ray ray = new Ray(new Vector3(topLeftX + x, 100, topLeftZ - y), Vector3.down);
